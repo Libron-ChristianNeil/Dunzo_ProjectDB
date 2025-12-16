af0bdee99a74d5a433c5422c59c931636d70e6d8
jojseph <josephvictornovabos@gmail.com>
2025-12-16 23:24:38 +0800

    design need

--- frontend/src/components/dashboard-components/QuickTaskCard.jsx
+++ frontend/src/components/dashboard-components/QuickTaskCard.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 
-function QuickTaskCard({item}) {
+function QuickTaskCard({ item }) {
     return (
         <div className='flex flex-row justify-between border-b border-gray-200 py-4 items-center cursor-pointer hover:bg-gray-50 px-2'>
             <div className='flex flex-row gap-3 items-center'>
-                <input type="checkbox" className='cursor-pointer mb-2 mr-2 ml-3 h-4 w-4 accent-red-500'/>
-                    <div className='flex flex-col'>
-                        <span className='font-medium text-black text-lg'>{item.title}</span>
-                        <div className='flex flex-row gap-2 text-gray-500'>
-                            <div>
-                                <i className='far fa-calendar'></i>
-                                <span className='ml-1 text-sm '>Due: {item.due_date === '' ? 'No Due Date' : item.due_date}</span>
-                            </div>
-                                            
-                            <div>
-                                <i className='far fa-folder'></i>
-                                <span className='ml-1 text-sm '>Project: {item.project === '' ? "General" : item.project}</span>
-                            </div>
+                <div className='flex flex-col'>
+                    <span className='font-medium text-black text-lg'>{item.title}</span>
+                    <div className='flex flex-row gap-2 text-gray-500'>
+                        <div>
+                            <i className='far fa-calendar'></i>
+                            <span className='ml-1 text-sm '>Due: {item.due_date === '' ? 'No Due Date' : item.due_date}</span>
+                        </div>
+
+                        <div>
+                            <i className='far fa-folder'></i>
+                            <span className='ml-1 text-sm '>Project: {item.project === '' ? "General" : item.project}</span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -25,7 +24,7 @@ function QuickTaskCard({item}) {
                             ${item.priority === 'Medium' && 'text-yellow-500 bg-yellow-100'}
                             ${item.priority === 'Low' && 'text-green-500 bg-green-100'}`}>
                 {item.priority}
-            </div>                                
+            </div>
         </div>
     )
 }
